8d8c5a3c8918a37c2190eab396b25d0c714af6d1
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2023-01-08 17:47:31 -0300

    Adding summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LateralBarsComponent } from './lateral-bars/lateral-bars.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LateralBarsComponent
+    LateralBarsComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule
