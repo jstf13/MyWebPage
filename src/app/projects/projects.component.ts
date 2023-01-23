238103913d46a18830f8a76ab7d34ee20bf6495d
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2023-01-23 17:51:57 -0300

    Saving last changes for projects IVe worked

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Card {
+  title: string;
+  link: string;
+  description: string;
+  languages: string[];
+}
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProjectsComponent {
+  cards: Card[] = [
+    {
+      title: 'Project 1',
+      link: 'https://project1.com',
+      description: 'Description of project 1',
+      languages: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+      title: 'Project 2',
+      link: 'https://project2.com',
+      description: 'Description of project 2',
+      languages: ['TypeScript', 'React', 'Node.js']
+    },
+  ];
 }
