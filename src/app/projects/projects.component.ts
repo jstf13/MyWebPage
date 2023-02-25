86654c73f25ce39b91affd90f5864333ecd1e898
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2023-02-25 15:12:44 -0300

    Better styles for projects cards

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -15,16 +15,28 @@ interface Card {
 export class ProjectsComponent {
   cards: Card[] = [
     {
-      title: 'Project 1',
-      link: 'https://project1.com',
-      description: 'Description of project 1',
-      languages: ['JavaScript', 'HTML', 'CSS']
+      title: 'Sons of Gods',
+      link: 'https://github.com/Misos14/SonsOfGodsWeb',
+      description: 'A main page presents a preview of some of the outstanding NFTs, together with information about its creator, the creation date and the sale price.',
+      languages: ['TypeScript', 'HTML', 'CSS']
     },
     {
-      title: 'Project 2',
-      link: 'https://project2.com',
-      description: 'Description of project 2',
-      languages: ['TypeScript', 'React', 'Node.js']
+      title: 'Design 1 APP',
+      link: 'https://github.com/ORT-DA2/201595-222371-209316',
+      description: 'The web application for multiple pharmacies is a platform designed to facilitate the management of various pharmacies in one place.',
+      languages: ['C#, ', 'HTML', 'CSS', 'JavaScript', 'TypeScript']
+    },
+    {
+      title: 'Auto Hot key',
+      link: 'https://github.com/jstf13/AutoHoyKey',
+      description: 'Shortcuts and automatization for boring tasks in WINDOWS',
+      languages: ['JavaScript', 'AutoHotKey']
+    },
+    {
+      title: 'Algorithms',
+      link: 'https://github.com/jstf13/Algoritmos_2.0',
+      description: 'This repository contains a collection of search and optimization algorithms, such as backtracking, greedy, and others, for applications in various fields.',
+      languages: ['C++']
     },
   ];
 }
