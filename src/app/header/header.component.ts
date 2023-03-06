bd39e290e870d179e58d4ae77eb48ec6471fde87
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2023-03-06 11:09:27 -0300

    fixing responsive styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, HostListener } from '@angular/core';
 import { ScrollService } from '../services/scroll.service';
 import { DarkModeService } from '../services/dark-mode.service';
 
@@ -10,11 +10,12 @@ import { DarkModeService } from '../services/dark-mode.service';
 export class HeaderComponent implements OnInit {
   menuOpen = false;
 
-constructor(private scrollService: ScrollService,
-           public darkModeService: DarkModeService) {}
+  constructor(
+    private scrollService: ScrollService,
+    public darkModeService: DarkModeService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   @Output() scrollTo: EventEmitter<string> = new EventEmitter();
 
@@ -24,12 +25,33 @@ constructor(private scrollService: ScrollService,
   }
 
   toggleMenu() {
-    let menuItemsList = document.getElementById("menu-items-list");
-    if(menuItemsList == null) return;
-  
-    if(menuItemsList.classList.contains("active"))
-      menuItemsList.classList.remove("active");
-     else 
-      menuItemsList.classList.add("active");
-  } 
+    const menuItemsList = document.getElementById('menu-items-list');
+    if (menuItemsList == null) return;
+
+    if (menuItemsList.classList.contains('active')) {
+      menuItemsList.classList.remove('active');
+    } else {
+      menuItemsList.classList.add('active');
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const menuItemsList = document.getElementById('menu-items-list');
+    const toggleButton = document.querySelector('.menu-toggle');
+
+    // Check if the click target is the menu or toggle button
+    if (
+      menuItemsList?.contains(target) ||
+      toggleButton?.contains(target)
+    ) {
+      return;
+    }
+
+    // Close the menu if it's open
+    if (menuItemsList?.classList.contains('active')) {
+      this.toggleMenu();
+    }
+  }
 }
